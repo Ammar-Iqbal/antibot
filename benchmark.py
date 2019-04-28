9dc082ca81e4eb358a874a3296393a999b5e98d1
Anderson Chaves Faria <andercfaria@hotmail.com>
2019-04-28 02:49:45 -0300

    more classifiers

--- benchmark.py
+++ benchmark.py
@@ -5,97 +5,16 @@ from sklearn.impute import SimpleImputer
 from sklearn.svm import SVC, LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 import comparison as comp
 import visualization as visual
 import time
-import matplotlib.pyplot as plt
-from sklearn.model_selection import learning_curve
-from sklearn.model_selection import ShuffleSplit
-
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate a simple plot of the test and training learning curve.
-
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-
-    title : string
-        Title for the chart.
-
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 3-fold cross-validation,
-          - integer, to specify the number of folds.
-          - :term:`CV splitter`,
-          - An iterable yielding (train, test) splits as arrays of indices.
-
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-
-    n_jobs : int or None, optional (default=None)
-        Number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-
-    train_sizes : array-like, shape (n_ticks,), dtype float or int
-        Relative or absolute numbers of training examples that will be used to
-        generate the learning curve. If the dtype is float, it is regarded as a
-        fraction of the maximum size of the training set (that is determined
-        by the selected validation method), i.e. it has to be within (0, 1].
-        Otherwise it is interpreted as absolute sizes of the training sets.
-        Note that for classification the number of samples usually have to
-        be big enough to contain at least one sample from each class.
-        (default: np.linspace(0.1, 1.0, 5))
-    """
-    plt.figure()
-    plt.title(title)
-    if ylim is not None:
-        plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    plt.grid()
-
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-
-    plt.legend(loc="best")
-    return plt
-
 
 # Função para ler os dados do CSV
 def load_antibot():
@@ -113,7 +32,7 @@ def load_antibot():
     'isbot']
     
   # Lendo todos os dados
-  ds = pd.read_csv('antibot.data', sep=',', header=None, names=cols)
+  ds = pd.read_csv('antibot2.data', sep=',', header=None, names=cols)
   
   # Atributos em X
   X = ds.iloc[:, :-1]
@@ -129,8 +48,13 @@ def load_antibot():
 datasets = []
 datasets.append(load_antibot())
 
-
 # Configurando os parâmetros dos classificadores
+perceptron_params = {'perceptron__tol': [0.0001, 0.001, 0.01]}
+mlp_params = {'mlp__hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)], 'mlp__activation': ['tanh', 'relu'], 'mlp__alpha': [0.0001, 0.05], 'mlp__learning_rate': ['constant','adaptive'],}
+gaussian_nb_params = {}
+decision_tree_params = {}
+random_forest_params = {'randomforestclassifier__n_estimators': [1,3,5,7,9,11,13,15,17,19,21]}
+logistic_regression_params = {'logisticregress__C': [0.001,0.01,0.1,1,10,100]}
 linear_svc_params = {'linearsvc__C': np.linspace(0.5, 5., 10)}
 knn_params = {'kneighborsclassifier__n_neighbors': [1,3,5,7,9,11,13,15,17,19,21]}
 svc_linear_params = {'svc__kernel': ['linear'], 'svc__C': np.linspace(0.5, 5., 10)}
@@ -140,6 +64,12 @@ svc_sigmoid_params = {'svc__kernel': ['sigmoid'], 'svc__C': np.linspace(0.5, 5.,
 
 # Criando nossos pipelines
 pipes = []
+pipes.append(('perceptron', perceptron_params, make_pipeline(SimpleImputer(), MinMaxScaler(), Perceptron())))
+#pipes.append(('mlp', mlp_params, make_pipeline(SimpleImputer(), MinMaxScaler(), MLPClassifier())))
+pipes.append(('naive_bayes', gaussian_nb_params, make_pipeline(SimpleImputer(), MinMaxScaler(), GaussianNB())))
+pipes.append(('decision-tree', decision_tree_params, make_pipeline(SimpleImputer(), MinMaxScaler(), tree.DecisionTreeClassifier())))
+pipes.append(('random-forest', random_forest_params, make_pipeline(SimpleImputer(), MinMaxScaler(), RandomForestClassifier())))
+#pipes.append(('logistic-regression', logistic_regression_params, make_pipeline(SimpleImputer(), MinMaxScaler(), LogisticRegression())))
 pipes.append(('linear-svc', linear_svc_params, make_pipeline(SimpleImputer(), MinMaxScaler(), LinearSVC())))
 pipes.append(('knn', knn_params, make_pipeline(SimpleImputer(), MinMaxScaler(), KNeighborsClassifier())))
 pipes.append(('svc-linear', svc_linear_params, make_pipeline(SimpleImputer(), MinMaxScaler(), SVC())))
@@ -162,9 +92,7 @@ for ds_name, X, y in datasets:
     # Salvando o tempo para benchmark 
     tempo_inicial = time.time()
     # Procurando os melhores parâmetros para cada algoritmo
-    intermed = GridSearchCV(pipe, params, cv=skf)
-    # Calculando o fit da melhor tentativa
-    best_attemp = intermed.fit(X, y)
+    best_attemp = GridSearchCV(pipe, params, cv=skf).fit(X, y)
     # Extraindo os parametros da melhor tentativa
     best_params = best_attemp.best_params_
     bestparams.append(best_params)
@@ -176,10 +104,7 @@ for ds_name, X, y in datasets:
     # Computando tempo de execução
     tempo_total = time.time() - tempo_inicial
     # Retornando resultados
-    # contém erro por enquanto...
-    #plot_learning_curve(pipe, "Curva de aprendizado", X, y, ylim=(0.7, 1.01), cv=skf)
-    #plt.show()
-    print("[%s] = taxa de acerto: %.3f +- %.3f, roc_auc: %.3f +- %.3f (%.3f (s))" % (clf_name, scores_acc.mean(), scores_acc.std(), scores_roc.mean(), scores_roc.std(), tempo_total))
+    print("[%s] = taxa de acerto: %.2f%% +- %.2f%%, roc_auc: %.2f%% +- %.2f%% (%.3f (s))" % (clf_name, scores_acc.mean() * 100, scores_acc.std() * 100, scores_roc.mean() * 100, scores_roc.std() * 100, tempo_total))
 np.savetxt("best_params", bestparams, fmt='%s', delimiter=",")
 comp.load_data(data)
 
