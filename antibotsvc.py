4e000fe221090160b3106b1fbfa3cb8a860c85ad
Anderson Chaves Faria <andercfaria@hotmail.com>
2019-04-28 23:19:42 -0300

    usando a precisão para comparar com o fim

--- antibotsvc.py
+++ antibotsvc.py
@@ -1,6 +1,10 @@
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_score
 from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.exceptions import DataConversionWarning
+import warnings
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 import numpy as np
 import pandas as pd
 import time
@@ -45,16 +49,21 @@ scaler.fit(X)
 X = scaler.transform(X)
 tempo_inicial = time.time()
 # Best Params
-model = SVC(C=3.5, degree=2, gamma='scale', kernel='poly')
+model = SVC(C=3.0, degree=2, gamma='scale', kernel='poly')
 model.fit(X, y)
 scores_acc = cross_val_score(model, X, y, cv=skf, scoring='accuracy')
-scores_roc = cross_val_score(model, X, y, cv=skf, scoring='roc_auc')
+scores_prec = cross_val_score(model, X, y, cv=skf, scoring='precision')
 tempo_total = time.time() - tempo_inicial
-print("taxa de acerto: %.2f%% +- %.2f%%, roc_auc: %.2f%% +- %.2f%% (%.3f (s))" % (scores_acc.mean() * 100, scores_acc.std() * 100, scores_roc.mean() * 100, scores_roc.std() * 100, tempo_total))
+print("taxa de acerto: %.2f%% +- %.2f%%, precisão: %.2f%% +- %.2f%% (%.3f (s))" % (scores_acc.mean() * 100, scores_acc.std() * 100, scores_prec.mean() * 100, scores_prec.std() * 100, tempo_total))
 
 # Time to test what we have learned.
+print("Testando o que foi aprendido.")
 X_t, y_t = load_antibot("antibot2")
 scaler.fit(X_t)
 X_t = scaler.transform(X_t)
-print(model.predict(X_t))
-print("Testando o que foi aprendido.")
+score = model.score(X_t, y_t)
+print("Taxa de acerto: %.2f" % (score * 100))
+predictions = model.predict(X_t)
+print(classification_report(y_t, predictions))
+
+
