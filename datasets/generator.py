bd6224e1e194b4bf556ec6dffc11d48cfd750304
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-04-28 21:01:17 -0300

    Heal threshold not optional

--- datasets/generator.py
+++ datasets/generator.py
@@ -61,46 +61,46 @@ class AntibotDataset:
     def collected_items(self):
         # Sabemos que metade dos bots coletam itens automaticamente
         some_bots = round(self.bot_players / 2)
-        some_bots_items = dist.positive(
+        some_bots_collected_items = dist.positive(
             np.random.normal, (20, 4, some_bots)).round()
 
-        # Enquanto a outra metade dos bots não utiliza o recurso
+        # Enquanto a outra metade dos bots não utiliza este recurso
         other_bots = self.bot_players - some_bots
-        other_bots_items = dist.positive(
+        other_bots_collected_items = dist.positive(
             np.random.normal, (8, 4, other_bots)).round()
 
         # Assim como os jogadores justos
-        players_items = dist.positive(
+        players_collected_items = dist.positive(
             np.random.normal, (8, 4, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_items, other_bots_items)), players_items)
+        return (np.concatenate((some_bots_collected_items, other_bots_collected_items)), players_collected_items)
 
     # Tempo médio entre um item aparecer no cenário e ser coletado
     def avg_time_to_collect_item(self):
         # Sabemos que metade dos bots coletam itens automaticamente assim que estes aparecem no cenário
         some_bots = round(self.bot_players / 2)
-        some_bots_reaction_time = dist.positive(
+        some_bots_avg_time = dist.positive(
             np.random.normal, (600, 50, some_bots)).round()
 
         # E metade desses bots utilizam algum atraso fixo para reagir
         half_some_bots = round(some_bots / 2)
-        some_bots_reaction_time[half_some_bots:] += np.random.uniform(
+        some_bots_avg_time[half_some_bots:] += np.random.uniform(
             1, 10, half_some_bots).round() * 100
 
         # Enquanto a outra metade dos bots não utiliza o recurso
         other_bots = self.bot_players - some_bots
-        other_bots_reaction_time = dist.positive(
+        other_bots_avg_time = dist.positive(
             np.random.normal, (1500, 250, other_bots)).round()
 
         # Assim como os jogadores justos
-        players_reaction_time = dist.positive(
+        players_avg_time = dist.positive(
             np.random.normal, (1500, 250, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_reaction_time, other_bots_reaction_time)), players_reaction_time)
+        return (np.concatenate((some_bots_avg_time, other_bots_avg_time)), players_avg_time)
 
     # Mediana das diferenças entre os tempos de coleta
     def delta_time_to_collect_item(self):
-        # Sabemos que metade dos bots coletam itens automaticamente sempre que aparecem no cenário
+        # Sabemos que metade dos bots coletam itens automaticamente sempre que estes aparecem no cenário
         some_bots = round(self.bot_players / 2)
         some_bots_delta_time = dist.positive(
             np.random.normal, (100, 25, some_bots)).round()
@@ -118,92 +118,83 @@ class AntibotDataset:
 
     # Mediana da quantidade de vida antes das curas começarem
     def heal_threshold(self):
-        # Sabemos que metade dos bots possuem um limiar para começar a cura
+        # Sabemos que metade dos bots possui um limiar alto para começar a cura
         some_bots = round(self.bot_players / 2)
+        some_bots_heal_threshold = dist.positive(
+            np.random.normal, (60, 2, some_bots)).round()
 
-        # Onde uma parte tem como estratégia utilizar um limiar alto
-        some_bots_half = round(some_bots / 2)
-        some_bots_half_heal = dist.positive(
-            np.random.normal, (60, 2, some_bots_half)).round()
-
-        # E outra parte tem como estratégia utilizar um limiar baixo
-        some_bots_other_half = some_bots - some_bots_half
-        some_bots_other_half_heal = dist.positive(
-            np.random.normal, (20, 2, some_bots_other_half)).round()
-
-        # Sendo que uma das duas estratégias pode ocorrer em qualquer um desses bots
-        some_bots_heal = np.random.permutation(np.concatenate(
-            (some_bots_half_heal, some_bots_other_half_heal)))
-
-        # Enquanto a outra metade não utiliza o recurso
+        # Enquanto a outra metade utiliza um limiar baixo para começar a cura
         other_bots = self.bot_players - some_bots
-        other_bots_heal = dist.positive(
-            np.random.normal, (50, 8, other_bots)).round()
+        other_bots_heal_threshold = dist.positive(
+            np.random.normal, (20, 2, other_bots)).round()
 
-        # Assim como os jogadores justos
-        players_heal = dist.positive(
+        # Ao passo que os jogadores justos variam em torno de meia vida
+        players_heal_threshold = dist.positive(
             np.random.normal, (50, 8, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_heal, other_bots_heal)), players_heal)
+        return (np.concatenate((some_bots_heal_threshold, other_bots_heal_threshold)), players_heal_threshold)
 
     # Tempo médio entre o último dano sofrido e o início da cura
     def avg_time_to_start_healing(self):
-        # Sabemos que metade dos bots começam a se curar assim que sofrem dano
+        # Sabemos que metade dos bots começam a se curar assim que sua vida chega a um limiar
         some_bots = round(self.bot_players / 2)
-        some_bots_reaction_time = dist.positive(
+        some_bots_avg_time = dist.positive(
             np.random.normal, (600, 50, some_bots)).round()
 
         # E metade desses bots utilizam algum atraso fixo para reagir
-        some_bots_half = round(some_bots / 2)
-        some_bots_reaction_time[-some_bots_half:] += np.random.uniform(
-            1, 10, some_bots_half).round() * 100
-
-        # Enquanto a outra metade não utiliza o recurso
         other_bots = self.bot_players - some_bots
-        other_bots_reaction_time = dist.positive(
-            np.random.normal, (2500, 450, other_bots)).round()
+        other_bots_avg_time = dist.positive(
+            np.random.normal, (600, 50, other_bots)).round()
+        other_bots_avg_time += np.random.uniform(
+            1, 10, other_bots).round() * 100
 
-        # Assim como os jogadores justos
-        players_reaction_time = dist.positive(
+        bots_avg_time = np.random.permutation(np.concatenate(
+            (some_bots_avg_time, other_bots_avg_time)))
+
+        # Ao passo que os jogadores justos variam em torno de 2500 ms
+        players_avg_time = dist.positive(
             np.random.normal, (2500, 450, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_reaction_time, other_bots_reaction_time)), players_reaction_time)
+        return (bots_avg_time, players_avg_time)
 
     # Mediana das diferenças entre os tempos de último dano sofrido e início da cura
     def delta_time_to_start_healing(self):
-        # Sabemos que metade dos bots sempre começam a se curar assim que sofrem dano
+        # Sabemos que metade dos bots sempre começam a se curar assim que sua vida chega a um limiar
         some_bots = round(self.bot_players / 2)
         some_bots_delta_time = dist.positive(
             np.random.normal, (1000, 100, some_bots)).round()
 
-        # Enquanto a outra metade não utiliza o recurso
+        # Enquanto a outra metade tem um comportamento errático
         other_bots = self.bot_players - some_bots
-        other_bots_delta_time = dist.positive(
-            np.random.normal, (1500, 300, other_bots)).round()
+        other_bots_delta_time = np.random.uniform(
+            500, 2000, other_bots).round()
 
-        # Assim como os jogadores justos
+        bots_delta_time = np.random.permutation(np.concatenate(
+            (some_bots_delta_time, other_bots_delta_time)))
+
+        # Ao passo que os jogadores justos são previsíveis apesar de variar bastante
         players_delta_time = dist.positive(
             np.random.normal, (1500, 300, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_delta_time, other_bots_delta_time)), players_delta_time)
+        return (bots_delta_time, players_delta_time)
 
     # Total de inimigos mortos
     def killed_enemies(self):
         # Sabemos que metade dos bots matam muitos monstros
         some_bots = round(self.bot_players / 2)
-        some_bots_kills = dist.positive(
+        some_bots_killed_enemies = dist.positive(
             np.random.normal, (20, 5, some_bots)).round()
 
         # Enquanto a outra metade não usa todo seu potencial
         other_bots = self.bot_players - some_bots
-        other_bots_kills = dist.positive(
+        other_bots_killed_enemies = dist.positive(
             np.random.normal, (12, 4, other_bots)).round()
 
         # E existem jogadores justos desde os muito ruins até os muito bons
-        players_kills = dist.positive(
+        players_killed_enemies = dist.positive(
             np.random.normal, (12, 8, self.fair_players)).round()
 
-        return (np.concatenate((some_bots_kills, other_bots_kills)), players_kills)
+        return (np.concatenate((some_bots_killed_enemies, other_bots_killed_enemies)), players_killed_enemies)
 
     # Tempo total sentindo fome
     def hungry_time(self):
